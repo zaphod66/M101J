18cbaf7033f67c7a048d15aa2bba18df56c447cc
Norbert Scheller <nscheller@actian.com>
2013-10-16 12:41:53 +0200

    Hello World, MongoDB, Freemarker, Spark

--- M101J/src/main/java/com/tengen/HelloWorldMongoDBSparkFreemarkerStyle.java
+++ M101J/src/main/java/com/tengen/HelloWorldMongoDBSparkFreemarkerStyle.java
@@ -1,29 +1,34 @@
 package com.tengen;
 
 import java.io.*;
-import java.util.*;
+import java.net.*;
+import com.mongodb.*;
 
-import freemarker.template.*;
 import spark.*;
+import freemarker.template.*;
 
-public class HelloWorldSparkFreemarkerStyle {
-    public static void main(String[] args) {
+public class HelloWorldMongoDBSparkFreemarkerStyle {
+    public static void main(String[] args) throws UnknownHostException {
         final Configuration config = new Configuration();
         config.setClassForTemplateLoading(HelloWorldSparkFreemarkerStyle.class, "/");
         
+        MongoClient client = new MongoClient(new ServerAddress("localhost", 27017));
+        
+        DB database = client.getDB("test");
+        final DBCollection collection = database.getCollection("hello");
+
         Spark.get(new Route("/") {
 
             @Override
             public Object handle(final Request request, final Response response) {
                 StringWriter writer = new StringWriter();
-                
-                Map<String,Object> helloMap = new HashMap<String,Object>();
-                helloMap.put("name", "Freemarker");
+
+                DBObject document = collection.findOne();
                 
                 try {
                     Template helloTemplate = config.getTemplate("hello.ftl");
                     
-                    helloTemplate.process(helloMap, writer);
+                    helloTemplate.process(document, writer);
                 } catch (Exception e) {
                     halt(500);  // telling spark to send error 500
                     e.printStackTrace();
@@ -32,6 +37,6 @@ public class HelloWorldSparkFreemarkerStyle {
                 return writer;
             }
             
-        });
+        });        
     }
 }
