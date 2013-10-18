e4a6ed7644184b69f6fa502f05463497bd1f5380
Norbert Scheller <nscheller@actian.com>
2013-10-18 16:46:26 +0200

    Find completed (count)

---
+++ M101J/src/main/java/com/tengen/week2/FindTest.java
@@ -0,0 +1,38 @@
+package com.tengen.week2;
+
+import java.net.*;
+import java.util.*;
+
+import com.mongodb.*;
+
+public class FindTest {
+    public static void main(String[] args) throws UnknownHostException {
+        MongoClient client = new MongoClient(new ServerAddress("localhost", 27017));
+        DB courseDB = client.getDB("course");
+        DBCollection coll = courseDB.getCollection("findTest");
+        
+        coll.drop();
+        
+        for (int i = 0; i < 10; i++) {
+            coll.insert(new BasicDBObject("x", new Random().nextInt(100)));
+        }
+        
+        System.out.println("findOne()");
+        DBObject one = coll.findOne();
+        System.out.println(one);
+        
+        System.out.println("find()");
+        DBCursor cursor = coll.find();
+        try {
+            while (cursor.hasNext()) {
+                DBObject cur = cursor.next();
+                System.out.println(cur);
+            }
+        } finally {
+            cursor.close();
+        }
+        
+        System.out.println("count()");
+        
+    }
+}
